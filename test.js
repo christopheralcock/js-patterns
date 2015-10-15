d54a718ee7accff615079a9768031f1d37f5a498
Christopher Alcock <christopheralcock@Christophers-MacBook-Pro-2.local>
2015-10-15 19:47:56 +0100

    single var

---
+++ test.js
@@ -0,0 +1,10 @@
+// you can actually just say "var" once, as long as you string all the statements together with commas
+function func() {
+  var a = 1,
+  b = 2,
+  sum = a + b,
+  myobject = {},
+  i,
+  j;
+ // function body...
+};
